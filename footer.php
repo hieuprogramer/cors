2acb1aa798394d2dcabe35dc8561b3f7cd6ecabe
Hieu Dao <xuanhieua5tt@gmail.com>
2021-12-29 05:32:49 +0700

    some message

--- footer.php
+++ footer.php
@@ -3,5 +3,5 @@
         <span style="margin-left: 200px;">Vulerable Web - PTIT-2021</span>
     </div>
 </footer>
-<script src="vendor/jquery-3.4.1.min.js"></script>
+<script src="js/jquery-3.4.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
